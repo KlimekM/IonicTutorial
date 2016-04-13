c664e2a1d563c62e02689f22aa3a1f502749592b
KlimekM <mklimek15@gmail.com>
2016-04-13 15:28:14 -0500

    Add logout functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -61,6 +61,7 @@ Controller for the favorites page
 */
 .controller('FavoritesCtrl', function($scope, $window, User) {
 	$scope.favorites = User.favorites;
+	$scope.username = User.username;
 
 	$scope.removeSong = function(song, index) {
 		User.removeSongFromFavorites(song, index);
@@ -75,8 +76,14 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope, User, Recommendations) {
+.controller('TabsCtrl', function($scope, $window, User, Recommendations) {
 	$scope.favCount = User.favoriteCount;
+
+	$scope.logout = function() {
+		User.destroySession();
+
+		$window.location.href = "index.html";
+	}
 
 	$scope.enteringFavorites = function() {
 		User.newFavorites = 0;
